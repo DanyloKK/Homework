ebc4569c18460d270d29a86ed9e615d3bc750e30
Your Name <you@example.com>
2024-10-06 18:53:07 +0300

    Make homework number 5

--- My.js
+++ My.js
@@ -1,8 +1,61 @@
 "use strict";
 
+let number = 20;
+for (; number <= 30; number = number + 0.5) {
+  console.log(number);
+}
+
+let dollar = 10;
+let grivna = dollar * 26;
+while (dollar <= 100) {
+  let grivna = dollar * 26;
+  console.log(grivna);
+  dollar += 10;
+}
+
+let N = +prompt(`Insert the number:`);
+
+for (let i = 1; i <= 100; i++) {
+  if (i * i < N) {
+    console.log(i);
+  } else {
+    break;
+    alert(`Your number not exceeding the 100!`);
+  }
+}
+
+let num = prompt(`Insert the number to check!`);
+
+function checkNumber() {
+  if (num < 2) {
+    return alert(`Number is to small!`);
+  }
+
+  for (let i = 3; i < num; i++) {
+    if (num % i === 0) {
+      alert(`${num} — not a simple number!`);
+      return;
+    }
+  }
+  alert(`${num} — simple число!`);
+}
+
+checkNumber();
+
+/*
+let array = [];
+for (let number = 2; number <= 10; number++) {
+  let innerArray = [];
+  for (let num = 1; num <= 10; num++) {
+    let result = number * num;
+    innerArray.push(result);
+  }
+  array.push(innerArray);
+}
+console.log(array);
+
 let userName = prompt(`What is your name?`);
 alert(`Hello ${userName} how are you?`);
-
 
 let userNumber = prompt(`Insert any three-digit number:`);
 
@@ -10,82 +63,247 @@ let numberOne = userNumber[0];
 let numberTwo = userNumber[1];
 let numberThree = userNumber[2];
 
-if (numberOne === numberTwo && numberTwo === numberThree){
-    console.log("All numbers are the same!")
-}else{
-    console.log("Numbers are different!")
+if (numberOne === numberTwo && numberTwo === numberThree) {
+  console.log("All numbers are the same!");
+} else {
+  console.log("Numbers are different!");
 }
-  if(numberOne === numberTwo && numberTwo === numberThree){
-      console.log("All numbers are equal again!!!!");
-  }
-else if (numberOne === numberTwo){
-    console.log("Number 1 are equal to number 2");
-}else if (numberTwo === numberThree){
-    console.log("Number 2 are equal to number 3");
-}else if (numberOne === numberThree){
-    console.log("Number 1 are equal to number 3");
+if (numberOne === numberTwo && numberTwo === numberThree) {
+  console.log("All numbers are equal again!!!!");
+} else if (numberOne === numberTwo) {
+  console.log("Number 1 are equal to number 2");
+} else if (numberTwo === numberThree) {
+  console.log("Number 2 are equal to number 3");
+} else if (numberOne === numberThree) {
+  console.log("Number 1 are equal to number 3");
 }
-
-
-
 
 let userBirth = prompt(`What is your date of birth?`);
 let userResidence = prompt(`What city do you live in?`);
 let userFavouriteSport = prompt(`What is your favourite sport?`);
 let userAge = 2024 - Number(userBirth);
-if ( userAge !== null && userAge <= 100 && userAge > 18 && userBirth !== null && userBirth !== ""  && userResidence !== null && userResidence !== "" && userFavouriteSport !== null && userFavouriteSport !== ""){
-
-    alert(`Your age is ${userAge}`);
-    alert(`You live in ${userResidence}`);
-    alert(`Your favorite sport is ${userFavouriteSport}`);
-
-}else {
-    if (userBirth === null || userBirth === ""){
-        alert("Too bad you did not want to insert your date of birth:((")
-    }else if (userResidence === null || userResidence === ""){
-        alert("Too bad you did not want to insert your place of living:((")
-    }else if (userFavouriteSport === null || userFavouriteSport === ""){
-        alert("Too bad you did not want to insert your favorite sport:((")
-    }
+if (
+  userAge !== null &&
+  userAge <= 100 &&
+  userAge > 18 &&
+  userBirth !== null &&
+  userBirth !== "" &&
+  userResidence !== null &&
+  userResidence !== "" &&
+  userFavouriteSport !== null &&
+  userFavouriteSport !== ""
+) {
+  alert(`Your age is ${userAge}`);
+  alert(`You live in ${userResidence}`);
+  alert(`Your favorite sport is ${userFavouriteSport}`);
+} else {
+  if (userBirth === null || userBirth === "") {
+    alert("Too bad you did not want to insert your date of birth:((");
+  } else if (userResidence === null || userResidence === "") {
+    alert("Too bad you did not want to insert your place of living:((");
+  } else if (userFavouriteSport === null || userFavouriteSport === "") {
+    alert("Too bad you did not want to insert your favorite sport:((");
+  }
 }
-
 
 // Додаткове дз;
 
 let sport = prompt(`What is your favorite sport?`);
 
 let favoriteSport = {
-    box: "Mike Tyson",
-    football: "Lionel Messi",
-    swimming: "Mike Felps"
+  box: "Mike Tyson",
+  football: "Lionel Messi",
+  swimming: "Mike Felps",
+};
+
+if (sport !== null && sport !== "") {
+  if (favoriteSport[sport.toLowerCase()]) {
+    alert(`Cool! Want to became a ${favoriteSport[sport.toLowerCase()]}?`);
+  } else {
+    alert("Sorry we dont have info about this kind of sport((");
+  }
+} else {
+  alert("Too bad u didnt choose any type of sport((");
 }
 
-
-if (sport !== null && sport !== ""){
-    if (favoriteSport[sport.toLowerCase()]){
-        alert(`Cool! Want to became a ${favoriteSport[sport.toLowerCase()]}?`);
-    }else {
-        alert("Sorry we dont have info about this kind of sport((")
-    }
-}else {
-    alert("Too bad u didnt choose any type of sport((")
+let numOrStr = prompt("input number or string");
+console.log(numOrStr);
+switch (numOrStr) {
+  case numOrStr === null:
+    console.log("ви скасували");
+    break;
+  case numOrStr.trim() === "":
+    console.log("Empty String");
+    break;
+  case isNaN(+numOrStr):
+    console.log(" number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
 }
 
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-switch (numOrStr){
-    case (numOrStr === null):
-        console.log('ви скасували');
-        break
-    case( numOrStr.trim() === '' ):
-        console.log('Empty String');
-        break
-    case (isNaN( +numOrStr)):
-        console.log(' number is Ba_NaN');
-        break
-    default:
-        console.log('OK!');
+let sport1 = "футбол";
+let champion1 = "Ліонель Мессі";
 
+let sport2 = "теніс";
+
+let champion2 = "Роджер Федерер";
+
+let sport3 = "баскетбол";
+
+let champion3 = "Леброн Джеймс";
+
+// Запрашиваем у пользователя, какой вид спорта ему нравится
+let userSport = prompt(
+  "Який твій улюблений вид спорту? (футбол, теніс, баскетбол)",
+);
+userSport = userSport.toLowerCase();
+
+// Проверяем, какой вид спорта ввел пользователь и выводим сообщение
+if (userSport === sport1) {
+  alert(`Круто! Хочеш стати ${champion1}?`);
+} else if (userSport === sport2) {
+  alert(`Круто! Хочеш стати ${champion2}?`);
+} else if (userSport === sport3) {
+  alert(`Круто! Хочеш стати ${champion3}?`);
+} else {
+  alert("На жаль, цей вид спорту не в списку.");
 }
 
+let a = 25;
+let b = Math.floor(a / 10); // Перша цифра
+let c = a % 10; // Друга цифра
 
+if (b > c) {
+  console.log("B");
+} else if (c > b) {
+  console.log("C");
+} else {
+  console.log("Цифри однакові");
+}
+
+let number = 24;
+
+if (number % 2 === 0) {
+  console.log(23);
+} else {
+  console.log(25);
+}
+
+let getNumber = 25;
+let getNum = Math.floor(getNumber / 10);
+let getNum1 = getNumber % 10;
+if (getNum1 % 2 === 0) {
+  console.log("Парное");
+} else {
+  console.log("Не парное");
+}
+
+function getRange(v) {
+  if (v <= 500 && v >= 0) {
+    console.log("Входит");
+  } else {
+    console.log("Не входит");
+  }
+}
+
+getRange(0);
+
+let n = 0;
+do {
+  console.log(n);
+  n++;
+} while (n < 0);
+console.log(n);
+
+let object1 = {
+  name: "Danua",
+  age: 22,
+  hobbies: {
+    games: "GTA 5",
+    gym: "Powerlifting",
+  },
+  measurements: {
+    chest: "118 cm",
+    shoulders: "140 cm",
+    bicep: "44 cm",
+  },
+};
+
+for (let key in object1) {
+  console.log(key);
+}
+for (let key in object1.measurements) {
+  console.log(key);
+  console.log(object1.measurements[key]);
+}
+
+function constructor(name, age, weightt, heightt, hobbies) {
+  this.name = name;
+  this.age = age;
+  this.weightt = weightt;
+  this.heightt = heightt;
+  this.hobbies = {
+    sport: hobbies.sport,
+    music: hobbies.music,
+    reading: hobbies.reading,
+  };
+}
+
+const person = new constructor("Danil", 22, 94, 179, {
+  sport: "Football",
+  music: "Rock",
+  reading: "Sci-fi",
+});
+console.log(person);
+let num2 = 1.005 + Number.EPSILON;
+let num1 = Math.round(num2 * 100) / 100;
+
+console.log(num1);
+
+let value = 58 + "Про";
+if (isNaN(value)) {
+  console.log("Это null!!");
+}
+
+let array = [10, 58, 39, -150, 0];
+let checkArray = [...array];
+console.log(Math.max[checkArray]);
+
+let text = `Hello my name
+\t "is Danylo"
+\t\t and  name your is?`;
+let text1 = text.includes("ello", 3);
+console.log(text1);
+
+let userName = prompt("What is your name?");
+let userName1 = userName.toLowerCase();
+
+let userNames = {
+  ivan: 22,
+  volodya: 23,
+  danil: 30,
+  vasya: 46,
+};
+
+if (userNames[userName1]) {
+  alert(`You are ${userNames[userName1]} years old.`);
+} else {
+  alert("User not found.");
+}
+
+outer: for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    let input = prompt(`Значення в координатах (${i},${j})`, "");
+
+    if (!input) break outer;
+  }
+}
+alert("Готово!");
+
+let multiple = 1;
+for (let i = 1; i <= 4; i++) {
+  multiple = multiple * i;
+}
+console.log(multiple);
+*/
